d8805bbc40746b826976da3a82a1839a62065db8
swapnil shinde <creepydazze@gmail.com>
2021-11-16 14:39:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 consumer_key="hvJ3Mc5hfuw6FbuW0GwsrNail"
 consumer_secret="77g1My9L2oEyzY137okPsRAlB2hOjkLulyxZRJ00lbVMY8NKwe"
 callback_uri="oob"    
-spredsheet_url="https://docs.google.com/spreadsheets/d/1RycbJdJ5idFOIfjDeJMoMefvOaKuP1stkDrZJugfPpA/edit#gid=0"
+spredsheet_url="https://docs.google.com/spreadsheets/d/1MpmRXqDPKFBeJm-EejVrbLIsxvu3vPVUKDFmUSzFB58/edit#gid=0"
