cc6548ce981f969f03a168ee3013585424d61903
swapnil shinde <creepydazze@gmail.com>
2021-11-16 11:50:34 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key="hvJ3Mc5hfuw6FbuW0GwsrNail"
+consumer_secret="77g1My9L2oEyzY137okPsRAlB2hOjkLulyxZRJ00lbVMY8NKwe"
+callback_uri="oob"    
+spredsheet_url="https://docs.google.com/spreadsheets/d/1ZHbtut6H59CSz9SCyw6734OA1vqvXK3tqmY1aOzkuiE/edit?usp=sharing"
