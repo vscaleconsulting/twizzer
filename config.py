fc5d31666755c17a165122567703d61bba6b1b4a
swapnil shinde <creepydazze@gmail.com>
2021-11-16 15:24:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 consumer_key="hvJ3Mc5hfuw6FbuW0GwsrNail"
 consumer_secret="77g1My9L2oEyzY137okPsRAlB2hOjkLulyxZRJ00lbVMY8NKwe"
 callback_uri="oob"    
-spredsheet_url=""
+spredsheet_url="https://docs.google.com/spreadsheets/d/1MHNq0lG8iK_H18ulxWQx0aOfYhUvyp5sSunCYYhuAuc/edit#gid=0"
