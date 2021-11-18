d6c80dfdec2c8ff1e71aebfdb5ad7b50c2234fe2
swapnil shinde <creepydazze@gmail.com>
2021-11-18 09:13:37 +0530

    Update twizzer.py

--- twizzer.py
+++ twizzer.py
@@ -102,7 +102,7 @@ class VscaleTwitterBot():
     spreadsheet_response = gsheet.values_get(f"Form Responses 1!A{self.scraped_till}:A")
 #     output_sheet_filled_till_response = gsheet.values_get(f"Output!A1:A")
     
-#     if 'values' in output_sheet_filled_till_response:
+#     if 'values' in output_sheet _filled_till_response:
 #       self.scraped_till += len(output_sheet_filled_till_response['values']) 
 #       print(output_sheet_filled_till_response['values'])  
     
