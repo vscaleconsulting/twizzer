ab6e4fd0b34aaeef5a7fb1624c3a30fcd9958d4c
swapnil shinde <creepydazze@gmail.com>
2022-06-09 23:25:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-consumer_key="hvJ3Mc5hfuw6FbuW0GwsrNail"
-consumer_secret="77g1My9L2oEyzY137okPsRAlB2hOjkLulyxZRJ00lbVMY8NKwe"
+consumer_key=""
+consumer_secret=""
 callback_uri="oob"    
 spredsheet_url="https://docs.google.com/spreadsheets/d/1zXe1oK-m1MLPlfR5vceqMs-Jtz9LNbbDoLIfx4umicE/edit#gid=0"
